cb23a14c5dc3c66cf886e2469272f52d2296166d
Janis Bizkovskis <janis.bizkovskis@prakse.lv>
2019-05-05 19:35:29 +0200

    [master] Update QueryBuilderTest

--- tests/Unit/Dms/MySQL/QueryBuilder/QueryBuilderTest.php
+++ tests/Unit/Dms/MySQL/QueryBuilder/QueryBuilderTest.php
@@ -2,6 +2,7 @@
 
 namespace Janisbiz\LightOrm\Tests\Unit\Dms\MySQL\QueryBuilder;
 
+use Janisbiz\LightOrm\Dms\MySQL\Enum\CommandEnum;
 use Janisbiz\LightOrm\Dms\MySQL\QueryBuilder\QueryBuilder;
 use Janisbiz\LightOrm\Dms\MySQL\QueryBuilder\Traits;
 use Janisbiz\LightOrm\Dms\MySQL\Repository\AbstractRepository;
@@ -10,15 +11,6 @@ use Janisbiz\LightOrm\Tests\Unit\Dms\MySQL\QueryBuilder\Traits\AbstractTraitTest
 
 class QueryBuilderTest extends AbstractTraitTestCase
 {
-    const METHOD_INSERT = 'insert';
-    const METHOD_INSERT_IGNORE = 'insertIgnore';
-    const METHOD_REPLACE = 'replace';
-    const METHOD_FIND_ONE = 'findOne';
-    const METHOD_FIND = 'find';
-    const METHOD_UPDATE = 'update';
-    const METHOD_UPDATE_IGNORE = 'updateIgnore';
-    const METHOD_DELETE = 'delete';
-
     /**
      * @var \ReflectionMethod[]
      */
@@ -100,17 +92,250 @@ class QueryBuilderTest extends AbstractTraitTestCase
         );
     }
 
-    public function testBuildQuery()
+    /**
+     * @dataProvider buildQueryData
+     *
+     * @param string $command
+     * @param array $methods
+     * @param array $expected
+     * @param array $expectedToString
+     */
+    public function testBuildQuery($command, $methods, $expected, $expectedToString)
     {
+        foreach ($methods as $method => $value) {
+            switch ($method) {
+                case 'column':
+                case 'table':
+                case 'groupBy':
+                case 'limit':
+                case 'offset':
+                    $this->queryBuilder->$method($value);
+                    break;
+
+                case 'innerJoin':
+                case 'leftJoin':
+                case 'rightJoin':
+                case 'crossJoin':
+                case 'fullOuterJoin':
+                    $this->queryBuilder->$method($value[0], $value[1], !empty($value[2]) ? $value[2] : []);
+                    break;
+
+                case 'innerJoinAs':
+                case 'leftJoinAs':
+                case 'rightJoinAs':
+                case 'crossJoinAs':
+                case 'fullOuterJoinAs':
+                    $this->queryBuilder->$method($value[0], $value[1], $value[2], !empty($value[3]) ? $value[3] : []);
+                    break;
+
+                case 'where':
+                case 'whereIn':
+                case 'whereNotIn':
+                case 'having':
+                case 'value':
+                case 'onDuplicateKeyUpdate':
+                    $this->queryBuilder->$method($value[0], $value[1]);
+                    break;
+            }
+        }
+
+        $this->queryBuilder->command($command);
+
+        dump($this->queryBuilder->buildQuery());
+        dump($this->queryBuilder->toString());
+
+        $this->assertEquals(\implode(' ', $expected), $this->queryBuilder->buildQuery());
+        $this->assertEquals(\implode(' ', $expectedToString), $this->queryBuilder->toString());
     }
 
+    /**
+     * @return array
+     */
     public function buildQueryData()
     {
+        /** phpcs:disable */
         return [
             [
-
-            ]
+                CommandEnum::INSERT_INTO,
+                [
+                    'table' => 'table1',
+                    'value' => [
+                        'table1.column1',
+                        'table1Column1Value',
+                    ],
+                    'onDuplicateKeyUpdate' => [
+                        'table1.column1',
+                        'table1Column1Value2',
+                    ],
+                ],
+                [
+                    'INSERT INTO',
+                    'table1',
+                    '(table1.column1) VALUES (:Table1_Column1_Value)',
+                    'ON DUPLICATE KEY UPDATE table1.column1 = :Table1_Column1_OnDuplicateKeyUpdate',
+                ],
+                [
+                    'INSERT INTO',
+                    'table1',
+                    '(table1.column1) VALUES (\'table1Column1Value\')',
+                    'ON DUPLICATE KEY UPDATE table1.column1 = \'table1Column1Value2\'',
+                ],
+            ],
+            [
+                CommandEnum::SELECT,
+                [
+                    'column' => '*',
+                    'table' => 'table1',
+                    'innerJoin' => [
+                        'table2',
+                        'table2.id = table1.table2_id AND table2.column1 = :bindValue2',
+                        [
+                            'bindValue2' => 2,
+                        ],
+                    ],
+                    'leftJoin' => [
+                        'table3',
+                        'table3.id = table1.table3_id AND table3.column1 = :bindValue3',
+                        [
+                            'bindValue3' => 3,
+                        ],
+                    ],
+                    'rightJoin' => [
+                        'table4',
+                        'table4.id = table1.table4_id AND table4.column1 = :bindValue4',
+                        [
+                            'bindValue4' => 4,
+                        ],
+                    ],
+                    'crossJoin' => [
+                        'table5',
+                        'table5.id = table1.table5_id AND table5.column1 = :bindValue5',
+                        [
+                            'bindValue5' => 5,
+                        ],
+                    ],
+                    'fullOuterJoin' => [
+                        'table6',
+                        'table6.id = table1.table6_id AND table6.column1 = :bindValue6',
+                        [
+                            'bindValue6' => 6,
+                        ],
+                    ],
+                    'innerJoinAs' => [
+                        'table7',
+                        'table7_alias',
+                        'table7.id = table1.table7_id AND table7.column1 = :bindValue7',
+                        [
+                            'bindValue7' => 7,
+                        ],
+                    ],
+                    'leftJoinAs' => [
+                        'table8',
+                        'table8_alias',
+                        'table8.id = table1.table8_id AND table8.column1 = :bindValue8',
+                        [
+                            'bindValue8' => 8,
+                        ],
+                    ],
+                    'rightJoinAs' => [
+                        'table9',
+                        'table9_alias',
+                        'table9.id = table1.table9_id AND table9.column1 = :bindValue9',
+                        [
+                            'bindValue9' => 9,
+                        ],
+                    ],
+                    'crossJoinAs' => [
+                        'table10',
+                        'table10_alias',
+                        'table10.id = table1.table10_id AND table10.column1 = :bindValue10',
+                        [
+                            'bindValue10' => 10,
+                        ],
+                    ],
+                    'fullOuterJoinAs' => [
+                        'table11',
+                        'table11_alias',
+                        'table11.id = table1.table11_id AND table11.column1 = :bindValue11',
+                        [
+                            'bindValue11' => 11,
+                        ],
+                    ],
+                    'where' => [
+                        'table1.column1 = :table1Column1BindValue',
+                        [
+                            'table1Column1BindValue' => 'table1Column1BindValue'
+                        ]
+                    ],
+                    'whereIn' => [
+                        'table1.column2',
+                        [
+                            'table1Column2BindValue1',
+                            'table1Column2BindValue2',
+                        ]
+                    ],
+                    'whereNotIn' => [
+                        'table1.column3',
+                        [
+                            'table1Column3BindValue1',
+                            'table1Column3BindValue2',
+                        ]
+                    ],
+                    'groupBy' => 'table1.column1',
+                    'having' => [
+                        'table1.column1 = table2.column2 AND table1.column2 = :havingTable1Column2',
+                        [
+                            'havingTable1Column2' => 1.2,
+                        ]
+                    ],
+                    'limit' => 1,
+                    'offset' => 2,
+                ],
+                [
+                    'SELECT',
+                    '*',
+                    'FROM table1',
+                    'INNER JOIN table2 ON (table2.id = table1.table2_id AND table2.column1 = :bindValue2)',
+                    'LEFT JOIN table3 ON (table3.id = table1.table3_id AND table3.column1 = :bindValue3)',
+                    'RIGHT JOIN table4 ON (table4.id = table1.table4_id AND table4.column1 = :bindValue4)',
+                    'CROSS JOIN table5 ON (table5.id = table1.table5_id AND table5.column1 = :bindValue5)',
+                    'FULL OUTER JOIN table6 ON (table6.id = table1.table6_id AND table6.column1 = :bindValue6)',
+                    'INNER JOIN table7 AS table7_alias ON (table7.id = table1.table7_id AND table7.column1 = :bindValue7)',
+                    'LEFT JOIN table8 AS table8_alias ON (table8.id = table1.table8_id AND table8.column1 = :bindValue8)',
+                    'RIGHT JOIN table9 AS table9_alias ON (table9.id = table1.table9_id AND table9.column1 = :bindValue9)',
+                    'CROSS JOIN table10 AS table10_alias ON (table10.id = table1.table10_id AND table10.column1 = :bindValue10)',
+                    'FULL OUTER JOIN table11 AS table11_alias ON (table11.id = table1.table11_id AND table11.column1 = :bindValue11)',
+                    'WHERE table1.column1 = :table1Column1BindValue',
+                    'AND table1.column2 IN (:0_table1column2_In, :1_table1column2_In)',
+                    'AND table1.column3 NOT IN (:0_table1column3_NotIn, :1_table1column3_NotIn)',
+                    'GROUP BY table1.column1',
+                    'HAVING table1.column1 = table2.column2 AND table1.column2 = :havingTable1Column2',
+                    'LIMIT 1 OFFSET 2',
+                ],
+                [
+                    'SELECT',
+                    '*',
+                    'FROM table1',
+                    'INNER JOIN table2 ON (table2.id = table1.table2_id AND table2.column1 = 2)',
+                    'LEFT JOIN table3 ON (table3.id = table1.table3_id AND table3.column1 = 3)',
+                    'RIGHT JOIN table4 ON (table4.id = table1.table4_id AND table4.column1 = 4)',
+                    'CROSS JOIN table5 ON (table5.id = table1.table5_id AND table5.column1 = 5)',
+                    'FULL OUTER JOIN table6 ON (table6.id = table1.table6_id AND table6.column1 = 6)',
+                    'INNER JOIN table7 AS table7_alias ON (table7.id = table1.table7_id AND table7.column1 = 7)',
+                    'LEFT JOIN table8 AS table8_alias ON (table8.id = table1.table8_id AND table8.column1 = 8)',
+                    'RIGHT JOIN table9 AS table9_alias ON (table9.id = table1.table9_id AND table9.column1 = 9)',
+                    'CROSS JOIN table10 AS table10_alias ON (table10.id = table1.table10_id AND table10.column1 = 10)',
+                    'FULL OUTER JOIN table11 AS table11_alias ON (table11.id = table1.table11_id AND table11.column1 = 11)',
+                    'WHERE table1.column1 = \'table1Column1BindValue\'',
+                    'AND table1.column2 IN (\'table1Column2BindValue1\', \'table1Column2BindValue2\')',
+                    'AND table1.column3 NOT IN (\'table1Column3BindValue1\', \'table1Column3BindValue2\')',
+                    'GROUP BY table1.column1',
+                    'HAVING table1.column1 = table2.column2 AND table1.column2 = 1.2',
+                    'LIMIT 1 OFFSET 2',
+                ],
+            ],
         ];
+        /** phpcs:enable */
     }
 
     public function testGetEntity()
